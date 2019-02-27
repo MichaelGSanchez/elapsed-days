585c29124f27665160bbe75029f90a10afc05a1d
MichaelGSanchez <michaelgregorysanchez@gmail.com>
2019-02-27 07:21:43 -0700

    Attempt to multiply the days out to years. Then take the remainder and that should be the date which the overall number is referencing. I am close on a few of the dates, however, I do not have implementation of leap years.

--- src/edu/cnm/deepdive/util/DateOnly.java
+++ src/edu/cnm/deepdive/util/DateOnly.java
@@ -1,8 +1,27 @@
 package edu.cnm.deepdive.util;
 
+import org.omg.CORBA.PUBLIC_MEMBER;
+
 public class DateOnly {
 
   public static int elapsedDays(int year, int month, int day) {
+
+
+  if (year > 1970){
+    year = (year - 1970)*365;
+    day = year % 1970;
+    month = day/11;
+    return year + month + day;
+  }
+
+  if (year < 1970){
+    year = (year - 1970)*-365;
+    day = year % 1970;
+    month = day/11;
+    return year+month+day;
+  }
+
+
     return 0; // TODO Implement this method to return correct value.
   }
 
